ed80e533f81a92e4b7a99635748769127d8fdbee
HYKIM <ddh963963@naver.com>
2023-05-03 22:55:09 +0900

    Docs : Front Thymeleaf 퍼블리싱

--- src/main/resources/static/assets/vendor/email-form/validate.js
+++ src/main/resources/static/assets/vendor/email-form/validate.js
@@ -1,12 +1,7 @@
-/**
-* PHP Email Form Validation - v3.6
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll('.email-form');
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
@@ -18,7 +13,7 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
+        displayError(thisForm, '양식 작업 속성이 설정되지 않았습니다.!');
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
@@ -41,7 +36,7 @@
             }
           });
         } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+          displayError(thisForm, 'reCaptcha javascript API URL이 로드되지 않았습니다!')
         }
       } else {
         php_email_form_submit(thisForm, action, formData);
@@ -68,7 +63,7 @@
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+        throw new Error(data ? data : '양식 제출에 실패했으며 에서 반환된 오류 메시지가 없습니다.: ' + action);
       }
     })
     .catch((error) => {
@@ -78,7 +73,7 @@
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').innerHTML ="오류입니다. 나중에 시도해주세요";
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
