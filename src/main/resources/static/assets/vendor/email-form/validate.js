801796f70cb7d4d2254d4158781ce3e1a8e46c61
HYKIM <ddh963963@naver.com>
2023-05-10 13:17:22 +0900

    Feat : validation 수정

--- src/main/resources/static/assets/vendor/email-form/validate.js
+++ src/main/resources/static/assets/vendor/email-form/validate.js
@@ -1,80 +1,112 @@
 (function () {
-  "use strict";
+    "use strict";
 
-  let forms = document.querySelectorAll('.email-form');
+    let forms = document.querySelectorAll('.email-form');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
-      event.preventDefault();
+    forms.forEach(function (e) {
+        e.addEventListener('submit', function (event) {
+            event.preventDefault();
 
-      let thisForm = this;
+            let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, '양식 작업 속성이 설정되지 않았습니다.!');
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+            let action = thisForm.getAttribute('action');
 
-      let formData = new FormData( thisForm );
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error);
+            if (!action) {
+                displayError(thisForm, '양식 작업 속성이 설정되지 않았습니다.!');
+                return;
             }
-          });
-        } else {
-          displayError(thisForm, 'reCaptcha javascript API URL이 로드되지 않았습니다!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
-    });
-  });
+            thisForm.querySelector('.loading').classList.add('d-block');
+            thisForm.querySelector('.error-message').classList.remove('d-block');
+            thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-  function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : '양식 제출에 실패했으며 에서 반환된 오류 메시지가 없습니다.: ' + action);
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
-  }
+            let formData = new FormData(thisForm);
 
-  function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML ="오류입니다. 나중에 시도해주세요";
-    thisForm.querySelector('.error-message').classList.add('d-block');
-  }
+            const form = document.querySelector('.email-form');
+            const nameInput = form.querySelector('input[name="name"]');
+            const emailInput = form.querySelector('input[name="email"]');
+            const phoneInput = form.querySelector('input[name="phone"]');
+            const messageInput = form.querySelector('textarea[name="message"]');
+
+            const nameValue = nameInput.value.trim();
+            const emailValue = emailInput.value.trim();
+            const phoneValue = phoneInput.value.trim();
+            const messageValue = messageInput.value.trim();
+
+            const errors = [];
+
+            if (nameValue.length < 1 || nameValue.length > 10) {
+                errors.push('성함은 1글자 이상, 10글자 이하이어야 합니다.');
+                nameInput.classList.add('is-invalid');
+            } else {
+                nameInput.classList.remove('is-invalid');
+            }
+
+            if (emailValue.length < 4 || emailValue.length > 100) {
+                errors.push('이메일은 4글자 이상, 100글자 이하이어야 합니다.');
+                emailInput.classList.add('is-invalid');
+            } else {
+                emailInput.classList.remove('is-invalid');
+            }
+
+            if (phoneValue.length < 5 || phoneValue.length > 16) {
+                errors.push('연락받을 번호는 5글자 이상, 16글자 이하이어야 합니다.');
+                phoneInput.classList.add('is-invalid');
+            } else {
+                phoneInput.classList.remove('is-invalid');
+            }
+
+            if (messageValue.length < 1) {
+                errors.push('내용을 입력해주세요.');
+                messageInput.classList.add('is-invalid');
+            } else {
+                messageInput.classList.remove('is-invalid');
+            }
+
+            if (errors.length > 0) {
+                const errorText = errors.join('<br>');
+                form.querySelector('.loading').classList.remove('d-block');
+                form.querySelector('.error-message').innerHTML = errorText;
+                form.querySelector('.error-message').classList.add('d-block');
+                return;
+            }
+
+            php_email_form_submit(thisForm, action, formData);
+
+        });
+    });
+
+    function php_email_form_submit(thisForm, action, formData) {
+        fetch(action, {
+            method: 'POST',
+            body: formData,
+            headers: {'X-Requested-With': 'XMLHttpRequest'}
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    throw new Error(`${response.status} : ${response.statusText} ${response.url}`);
+                }
+            })
+            .then(data => {
+                thisForm.querySelector('.loading').classList.remove('d-block');
+                if (data.trim() == 'OK') {
+                    thisForm.querySelector('.sent-message').classList.add('d-block');
+                    thisForm.reset();
+                } else {
+                    throw new Error(data ? data : '양식 제출에 실패했으며 에서 반환된 오류 메시지가 없습니다.: ' + action);
+                }
+            })
+            .catch((error) => {
+                displayError(thisForm, error);
+            });
+    }
+
+    function displayError(thisForm, error) {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        thisForm.querySelector('.error-message').innerHTML = "오류입니다. 나중에 시도해주세요";
+        thisForm.querySelector('.error-message').classList.add('d-block');
+    }
 
 })();
