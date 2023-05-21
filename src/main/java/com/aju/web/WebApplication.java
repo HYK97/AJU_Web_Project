bf1407dbc3d541510e626c6559a1147e2ff0494d
HYKim <ddh963963@naver.com>
2023-05-21 22:36:59 +0900

    Build : mariadb 추가 및 War 빌드파일명령어 추가

--- src/main/java/com/aju/web/WebApplication.java
+++ src/main/java/com/aju/web/WebApplication.java
@@ -1,15 +1,26 @@
 package com.aju.web;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.Set;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class WebApplication {
+public class WebApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebApplication.class);
+	}
+
 
 }
